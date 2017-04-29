332d881795364b35302e366f04877bb279409579
Caleb Cartwright <caleb.cartwright@outlook.com>
2017-04-28 20:53:55 -0500

    added some missing files from my local that are in use in the VSTS User Service code that has already been checked in

--- test/unit/services/vsts-graph-api-user-service-tests.ts
+++ test/unit/services/vsts-graph-api-user-service-tests.ts
@@ -4,6 +4,7 @@ import Chai = require('chai');
 import request = require('request');
 import Sinon = require('sinon');
 
+import testHelpers = require('./../test-helpers');
 import VstsGraphApiUserService = require('./../../../src/services/vsts-graph-api-user-service');
 import VstsUser = require('./../../../src/models/vsts-user');
 
@@ -15,12 +16,25 @@ const assert = Chai.assert;
 suite('VSTS Graph API User Service Suite:', () => {
     const sandbox: Sinon.SinonSandbox = Sinon.sandbox.create();
     let requestGetStub: Sinon.SinonStub;
+    let vstsGraphApiUserService: VstsGraphApiUserService;
 
     setup(() => {
         requestGetStub = sandbox.stub(request, 'get');
+        vstsGraphApiUserService = new VstsGraphApiUserService();
     });
 
     teardown(() => {
         sandbox.restore();
+        vstsGraphApiUserService = null;
+    });
+
+    suite('getAADUsers Suite:', () => {
+        const errorMessageBase = 'Encountered an error while retrieving VSTS users from AAD. Error details: ';
+        const errorMessageDetails = '';
+        const errorMessage = errorMessageBase + errorMessageDetails;
+    });
+
+    suite('getAllUsers Suite:', () => {
+
     });
 });
