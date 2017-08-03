35e3a5f9ec2a83cc9652fb4083ecc96c5577651b
Caleb Cartwright <caleb.cartwright@outlook.com>
2017-08-02 22:08:40 -0500

    extracted string format validation functions into seperate file

---
+++ src/format-validator.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Determines whether or not the specified input string is a valid GUID format.
+ */
+export const isValidGuid = (input: string): boolean => {
+    if (!input) {
+        return false;
+    }
+
+    return new RegExp('^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$').test(input);
+};
+
+/**
+ * Determines whether or not the specified string is in a valid ISO date format.
+ * Note that decimal seconds are optional, but no more than 3 decimal points are supported.
+ *
+ * @param {string} input - The string to validate
+ * @returns {booean} - True if the input is a valid ISO string format, otherwise false.
+ */
+export const isValidIsoFormat = (input: string): boolean => {
+    if (!input) {
+        return false;
+    }
+
+    // For now limiting to no more than 3 decimal places to represent milliseconds.
+    return new RegExp('^(\\d{4})-(\\d{2})-(\\d{2})T((\\d{2}):(\\d{2}):(\\d{2}))(\\.\\d{1,3})?Z$').test(input);
+}
---
+++ test/unit/format-validator-tests.ts
@@ -0,0 +1,69 @@
+'use strict';
+
+import Chai = require('chai');
+
+import formatValidator = require('./../../src/format-validator');
+import testHelpers = require('./test-helpers');
+
+const assert = Chai.assert;
+
+/**
+ * Contains unit tests for the functions defined in {@link ./src/format-validator.ts}
+ */
+suite('Format Validator Suite:', () => {
+    suite('isValidGuid Suite:', () => {
+        test('Should return false when the input is null', () => {
+            assert.deepEqual(formatValidator.isValidGuid(null), false);
+        });
+
+        test('Should return false when the input is undefined', () => {
+            assert.deepEqual(formatValidator.isValidGuid(undefined), false);
+        });
+
+        test('Should return false when the input is an invalid guid', () => {
+            assert.deepEqual(formatValidator.isValidGuid('!@#$%'), false);
+        });
+
+        test('Should return true when the input is a valid guid format', () => {
+            assert.deepEqual(formatValidator.isValidGuid(testHelpers.sampleGuid), true);
+        });
+    });
+
+    suite('isValidIsoFormat Suite:', () => {
+        test('Should return false when the input is null', () => {
+            assert.deepEqual(formatValidator.isValidIsoFormat(null), false);
+        });
+
+        test('Should return false when the input is undefined', () => {
+            assert.deepEqual(formatValidator.isValidIsoFormat(undefined), false);
+        });
+
+        test('Should return false when the input is not a valid ISO formatted string', () => {
+            assert.deepEqual(formatValidator.isValidIsoFormat('abc230497*&%754'), false);
+        });
+
+        test('Should return true when the input is a valid ISO format with no decimals', () => {
+            assert.deepEqual(formatValidator.isValidIsoFormat(testHelpers.isoFormatNoDecimalString), true);
+        });
+
+        test('Should return false when the input contains an invalid decimal formatted ISO', () => {
+            assert.deepEqual(formatValidator.isValidIsoFormat(testHelpers.isoFormatInvalidDecimalString), false);
+        });
+
+        test('Should return true when the input is a valid ISO format with one decimal place', () => {
+            assert.deepEqual(formatValidator.isValidIsoFormat(testHelpers.isoFormatOneDecimalString), true);
+        });
+
+        test('Should return true when the input is a valid ISO format with two decimal places', () => {
+            assert.deepEqual(formatValidator.isValidIsoFormat(testHelpers.isoFormatTwoDecimalsString), true);
+        });
+
+        test('Should return true when the input is a valid ISO format with three decimal places', () => {
+            assert.deepEqual(formatValidator.isValidIsoFormat(testHelpers.isoFormatThreeDecimalsString), true);
+        });
+
+        test('Should return false when the input is a valid ISO format with four decimal places', () => {
+            assert.deepEqual(formatValidator.isValidIsoFormat(testHelpers.isoFormatFourDecimalsString), false);
+        });
+    });
+});
