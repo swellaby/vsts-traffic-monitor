0464ad2f598400af56a702cbbb55412e56f367ad
Caleb Cartwright <caleb.cartwright@outlook.com>
2018-04-29 14:11:44 -0500

    added our gulp-vsts-bump plugin and updated gulp config to utilize it. closes #35

--- gulp/tasks/version.js
+++ gulp/tasks/version.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const bump = require('gulp-bump');
-const fs = require('fs');
 const gulp = require('gulp');
 const gulpConfig = require('./../gulp-config');
-const jsonModify = require('gulp-json-modify');
+const vstsBump = require('gulp-vsts-bump');
 
 gulp.task('bump-vsts-task-extension-version', function () {
     return gulp.src(gulpConfig.vstsExtensionManifest)
@@ -13,14 +12,8 @@ gulp.task('bump-vsts-task-extension-version', function () {
 });
 
 gulp.task('bump-task-version', function () {
-    const taskManifest = JSON.parse(fs.readFileSync(gulpConfig.taskManifestFile));
-    const bumpedPatch = (parseInt(taskManifest.version.Patch) + 1).toString();
-
     return gulp.src(gulpConfig.taskManifestFile)
-        .pipe(jsonModify({
-            key: 'version.Patch',
-            value: bumpedPatch
-        }))
+        .pipe(vstsBump())
         .pipe(gulp.dest('./'));
 });
 
